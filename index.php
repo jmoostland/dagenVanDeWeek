5f72ceb8b01383172fad535a7606d271671c3939
jmoostland <jeanineoostland@gmail.com>
2017-10-24 15:05:28 +0200

    eufi

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+ $dagen = array("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag");
+
+        for($dag = 0; $dag < count ($dagen); $dag++){
+     echo $dagen [$dag];
+     echo "<br>";
+         }
+
+?>
